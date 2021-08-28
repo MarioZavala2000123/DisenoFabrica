0955eab9353549da9e815b9d88a86bbeb2da2a8f
MarioZavala2000123 <mrzavalat@alumno.uspg.edu.gt>
2021-08-28 11:17:38 -0600

    Creacion del proyecto

---
+++ src/main/java/edu/uspg/DisenoFabricaApplication.java
@@ -0,0 +1,13 @@
+package edu.uspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisenoFabricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisenoFabricaApplication.class, args);
+	}
+
+}
